af254eff33f344c9b544f4d1e8af54e5726f918d
Favour Israel Taiwo <princeisrael409@gmail.com>
2023-11-08 23:24:01 +0100

    finished landing page

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export default function MaxWidthWrapper({
+  children,
+  className,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={cn(`mx-auto w-full max-w-screen-xl px-2.5 md:20`, className)}
+    >
+      {children}
+    </div>
+  );
+}
